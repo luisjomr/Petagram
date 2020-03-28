49c47d595add55f5cbfbf873396311bb9da2ff3a
LuisJo <lmelendezruiz@gmail.com>
2020-03-28 05:32:46 -0500

    Actualizado al 28 de marzo de 2020

--- app/src/main/java/com/insightiquitos/petagram/MainActivity.java
+++ app/src/main/java/com/insightiquitos/petagram/MainActivity.java
@@ -1,6 +1,6 @@
-package com.example.luisjo.petagram;
+package com.insightiquitos.petagram;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
